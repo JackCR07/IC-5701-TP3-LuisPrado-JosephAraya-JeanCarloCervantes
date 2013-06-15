e0eae2f800000c9a55ea5b06607b7d1b391c7526
JackCR07 <jean.cervantes07@gmail.com>
2013-06-15 00:54:35 -0500

    Update semantico.h

--- semantico.h
+++ semantico.h
@@ -1,8 +1,23 @@
+/*******************************************************************************************
+ * Tecnologico de Costa Rica                                                               *
+ * Ingieneria en Computación                                                               *
+ * Compiladores e Interpretes                                                              *
+ * Analizador Semantico xhtml                                                              *
+ * Tarea Programada 3                                                                      *
+ * semantico.c                                                                             *
+ * Header que funciones necesitadas para realizar el analisis semantico                    *
+ *                                                                                         *
+ * Estudiantes: Joseph Araya Rojas                                                         *
+ *  			Luis Prado Rodríguez                                                       *
+ *				Jean Carlo Cervantes                                                       *
+ *                                                                                         *
+ * Profesor: Andrei Fuentes                                                                *
+ * *****************************************************************************************/
 
-
-int atributoValido(char* elemento, char* atributo, char* valorAtributo);
-void printErrorSemantico(char* elemento, char* atributo, char* valorAtributo);
-void printErrorSemanticoAtributoInvalido(char* elemento, char* atributo, char* valorAtributo);
+int atributoValido(char* elemento, char* atributo, char* valorAtributo, int filaAtri, int columnaAtri, int filaValor, int columnaValor);
+void printErrorSemantico(char* elemento, char* atributo, char* valorAtributo, int filaAtri, int columnaAtri, int filaValor, int columnaValor);
+void printErrorSemanticoAtributoInvalido(char* elemento, char* atributo, char* valorAtributo, int filaAtri, int columnaAtri, int filaValor, int columnaValor);
 int isExpReg(char* expReg, char* dato);
-int eventAtributtes(char* elemento, char* atributo, char* valorAtributo);
-int globalatributes(char* elemento, char* atributo, char* valorAtributo);
+int eventAtributtes(char* elemento, char* atributo, char* valorAtributo, int filaAtri, int columnaAtri, int filaValor, int columnaValor);
+int globalatributes(char* elemento, char* atributo, char* valorAtributo, int filaAtri, int columnaAtri, int filaValor, int columnaValor);
+
