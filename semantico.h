90d191bee1d1ce19469a2b609d71dfebbc23ffb0
JackCR07 <jean.cervantes07@gmail.com>
2013-06-14 18:42:53 -0600

    Create semantico.h

---
+++ semantico.h
@@ -0,0 +1,8 @@
+
+
+int atributoValido(char* elemento, char* atributo, char* valorAtributo);
+void printErrorSemantico(char* elemento, char* atributo, char* valorAtributo);
+void printErrorSemanticoAtributoInvalido(char* elemento, char* atributo, char* valorAtributo);
+int isExpReg(char* expReg, char* dato);
+int eventAtributtes(char* elemento, char* atributo, char* valorAtributo);
+int globalatributes(char* elemento, char* atributo, char* valorAtributo);
